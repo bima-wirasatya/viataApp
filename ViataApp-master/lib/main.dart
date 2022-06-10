7b1ab4e5ba6da117014ca6086bd9f15c4f362d0d
bima-wirasatya <bima.anggara143@gmail.com>
2022-06-10 16:07:54 +0700

    adding menu

--- ViataApp-master/lib/main.dart
+++ ViataApp-master/lib/main.dart
@@ -1,25 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:viata/view/SignIn/login.dart';
+import 'package:viata/view/home_page.dart';
+import 'package:viata/view/main_page.dart';
+import 'package:viata/view/splash.dart';
+import 'package:viata/view/google_sign_in.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+  static final String title = 'main page';
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+    create: (context) => GoogleSignInProvider(),
+    child: MaterialApp(
       debugShowCheckedModeBanner: false,
-      routes: {
-
-        //'/': (context) => SplashPage(),
-
-      },
-    );
-  }
+      title: title,
+      theme: ThemeData.light().copyWith(accentColor: Colors.indigo),
+      home: HomePage(),
+    ),
+  );
 }
