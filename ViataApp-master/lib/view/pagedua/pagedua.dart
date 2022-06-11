28556e914c161ab7a69d41e742310eaaf9c2aa5d
bima-wirasatya <bima.anggara143@gmail.com>
2022-06-11 20:24:33 +0700

    add explanation

--- ViataApp-master/lib/view/pagedua/pagedua.dart
+++ ViataApp-master/lib/view/pagedua/pagedua.dart
@@ -33,7 +33,7 @@ class PageDua extends StatelessWidget {
 //
 
                     Text(
-                      'Judul',
+                      'Kanker',
                       style: primaryTextStyle.copyWith(
                           fontSize: 20, fontWeight: medium),
                     ),
@@ -41,18 +41,80 @@ class PageDua extends StatelessWidget {
                       height: 20,
                     ),
                     Text(
-                      'Jakarta - Setelah beredar info penemuan Emmeril Kahn Mumtdz, KBRI Indonesia akhirnya konfirmasi bahwa jenazah tersebut benar putra Ridwan Kamil.',
+                      'Penyakit kanker pada kucing pun juga memiliki berbagai jenis tergantung pada satu area saja atau sudah menyebar ke seluruh tubuh. Jenis kanker yang terjadi pada kucing yaitu kanker kulit dan Lymphosarcoma atau limfoma (LSA).',
                       style: primaryTextStyle.copyWith(
                           fontSize: 18, fontWeight: medium),
                     ),
-
-
-
-
-
                   ],
                 ),
               ),
+
+              SizedBox(
+                height: 20,
+              ),
+
+
+              Container(
+                width: 300,
+                padding: EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+                margin: EdgeInsets.only(right: 16),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: primaryColor,
+                ),
+                child: Column(
+                  children: [
+
+                    Text(
+                      'Diabetes',
+                      style: primaryTextStyle.copyWith(
+                          fontSize: 20, fontWeight: medium),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      'Diabetes pada kucing adalah penyakit yang disebabkan oleh kekurangan hormon insulin atau tidak adanya respons memadai terhadap insulin. Umumnya diabetes terjadi setelah kucing mengonsumsi makanan tertentu. Karena kurangnya insulin, sistem pencernaan tidak bisa memecah makanan menjadi berbagai komponen, termasuk glukosa yang dibawa ke setiap sel dalam tubuh melalui aliran darah. ',
+                      style: primaryTextStyle.copyWith(
+                          fontSize: 18, fontWeight: medium),
+                    ),
+                  ],
+                ),
+              ),
+
+              SizedBox(
+                height: 20,
+              ),
+
+
+              Container(
+                width: 300,
+                padding: EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+                margin: EdgeInsets.only(right: 16),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: primaryColor,
+                ),
+                child: Column(
+                  children: [
+
+                    Text(
+                      'Cacing hati',
+                      style: primaryTextStyle.copyWith(
+                          fontSize: 20, fontWeight: medium),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      'Penyakit satu ini ditularkan melalui nyamuk yang sudah terinfeksi. Heartworm semakin dikenal sebagai penyebab utama masalah kesehatan pada kucing domestik. Penyakit ini menyebabkan timbulnya sakit pada paru-paru kucing. ',
+                      style: primaryTextStyle.copyWith(
+                          fontSize: 18, fontWeight: medium),
+                    ),
+                  ],
+                ),
+              ),
+
             ],
           ),
         ),
--- ViataApp-master/lib/view/pagetiga/pagetiga.dart
+++ ViataApp-master/lib/view/pagetiga/pagetiga.dart
@@ -33,7 +33,7 @@ class PageTiga extends StatelessWidget {
 
 
                     Text(
-                      'Judul',
+                      'Royal Canin Hair & Skin (Rp114.000)',
                       style: primaryTextStyle.copyWith(
                           fontSize: 20, fontWeight: medium),
                     ),
@@ -41,18 +41,80 @@ class PageTiga extends StatelessWidget {
                       height: 20,
                     ),
                     Text(
-                      'Jakarta - Setelah beredar info penemuan Emmeril Kahn Mumtdz, KBRI Indonesia akhirnya konfirmasi bahwa jenazah tersebut benar putra Ridwan Kamil.',
+                      'Makanan kucing jenis Royal Canin memang sudah dikenal di pasaran dengan kualitas dan harganya yang mahal, Royal Canin Hair & Skin digunakan untuk menghindari kucing dari masalah bulu yang rontok. Selain itu, produk yang berasal dari Perancis ini bisa melebatkan dan memanjangkan bulu kucing, baik kucing domestik maupun ras tertentu.',
                       style: primaryTextStyle.copyWith(
                           fontSize: 18, fontWeight: medium),
                     ),
-
-
-
-
-
                   ],
                 ),
               ),
+
+              SizedBox(
+                height: 20,
+              ),
+
+
+              Container(
+                width: 300,
+                padding: EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+                margin: EdgeInsets.only(right: 16),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: primaryColor,
+                ),
+                child: Column(
+                  children: [
+
+                    Text(
+                      'Makanan Felibite (Rp30.000)',
+                      style: primaryTextStyle.copyWith(
+                          fontSize: 20, fontWeight: medium),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      'Felibite dibuat dengan nutrisi yang seimbang sehingga stamina dan kekebalan tubuh kucing tetap terjaga. Terdapat pula vitamin Omega 3 dan Omega 6 yang bisa merangsang kesehatan bulu, taurin untuk kesehatan mata, dan yucca extract untuk mengurangi bau pada kotoran.',
+                      style: primaryTextStyle.copyWith(
+                          fontSize: 18, fontWeight: medium),
+                    ),
+                  ],
+                ),
+              ),
+
+              SizedBox(
+                height: 20,
+              ),
+
+
+              Container(
+                width: 300,
+                padding: EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+                margin: EdgeInsets.only(right: 16),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: primaryColor,
+                ),
+                child: Column(
+                  children: [
+
+                    Text(
+                      'Makanan Me-O (Rp48.000)',
+                      style: primaryTextStyle.copyWith(
+                          fontSize: 20, fontWeight: medium),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      'Me-O cat food dry  memiliki berbagai varian rasa. Untuk kucing dewasa mulai dari, sapi, ayam, ikan buntal tuna, dan sayuran. Sedangkan untuk anak kucing terdapat rasa seafood. Produk Me-O mengandung vitamin C, vitamin D, omega 3 dan 6, fosfor, dan kalsium.',
+                      style: primaryTextStyle.copyWith(
+                          fontSize: 18, fontWeight: medium),
+                    ),
+                  ],
+                ),
+              ),
+
             ],
           ),
         ),
